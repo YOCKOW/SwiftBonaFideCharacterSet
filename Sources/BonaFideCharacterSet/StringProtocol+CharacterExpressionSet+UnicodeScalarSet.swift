65acb5f35b6d25efdbbf774ee7e9a2e2cf40c6b7
YOCKOW <YOCKOW@users.noreply.github.com>
2018-09-21 14:13:21 +0900

    Extend `StringProtocol` to split with `UnicodeScalarSet`.

--- Sources/BonaFideCharacterSet/StringProtocol+CharacterExpressionSet+UnicodeScalarSet.swift
+++ Sources/BonaFideCharacterSet/StringProtocol+CharacterExpressionSet+UnicodeScalarSet.swift
@@ -1,5 +1,5 @@
 /* *************************************************************************************************
- StringProtocol+CharacterExpressionSet.swift
+ StringProtocol+CharacterExpressionSet+UnicodeScalarSet.swift
    © 2018 YOCKOW.
      Licensed under MIT License.
      See "LICENSE.txt" for more information.
@@ -23,6 +23,27 @@ extension StringProtocol {
   public func components<C>(separatedBy separator:CharacterExpressionSet<C>) -> [String]
     where C: CharacterExpression
   {
+    return self.split(separator:separator, omittingEmptySubsequences:false).map{ String($0) }
+  }
+}
+
+extension StringProtocol {
+  /// Returns the longest possible subsequences of the collection, in order,
+  /// around elements equal to the given element.
+  public func split(separator:UnicodeScalarSet,
+                    maxSplits:Int = Int.max,
+                    omittingEmptySubsequences:Bool = true) -> [String]
+
+  {
+    let array = self.unicodeScalars.split(maxSplits:maxSplits,
+                                          omittingEmptySubsequences:omittingEmptySubsequences,
+                                          whereSeparator:{ separator.contains($0) })
+    return array.map { String(String.UnicodeScalarView($0)) }
+  }
+  
+  /// Returns an array containing substrings from the string
+  /// that have been divided by characters in the given set.
+  public func components(separatedBy separator:UnicodeScalarSet) -> [String] {
     return self.split(separator:separator, omittingEmptySubsequences:false).map{ String($0) }
   }
 }
--- Tests/BonaFideCharacterSetTests/StringProtocol+CharacterExpressionSet+UnicodeScalarSet_Tests.swift
+++ Tests/BonaFideCharacterSetTests/StringProtocol+CharacterExpressionSet+UnicodeScalarSet_Tests.swift
@@ -1,5 +1,5 @@
 /* *************************************************************************************************
- StringProtocol+CharacterExpressionSetTests.swift
+ StringProtocol+CharacterExpressionSet+UnicodeScalarSet_Tests.swift
    © 2018 YOCKOW.
      Licensed under MIT License.
      See "LICENSE.txt" for more information.
@@ -10,11 +10,15 @@ import XCTest
 
 import Ranges
 
-final class StringProtocol_CharacterExpressionSetTests: XCTestCase {
+final class StringProtocol_CharacterExpressionSet_UnicodeScalarSet_Tests: XCTestCase {
   func testSplit() {
     let string = "AA01BB23CC45DD67"
     XCTAssertEqual(
       string.components(separatedBy:CharacterSet(charactersIn:"01234567890")),
+      string.components(separatedBy:PrecomposedCharacterSet(charactersIn:"0"..."9"))
+    )
+    XCTAssertEqual(
+      string.components(separatedBy:UnicodeScalarSet(unicodeScalarsIn:"0123456789")),
       string.components(separatedBy:PrecomposedCharacterSet(charactersIn:"0"..."9"))
     )
   }
